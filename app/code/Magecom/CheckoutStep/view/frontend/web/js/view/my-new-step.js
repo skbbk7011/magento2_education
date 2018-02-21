a3639c44f6a8bb48d221da801cd8de3502bbfc77
Sofiia Khizhnia <sofabbk@gmail.com>
2018-02-21 14:35:11 +0200

    add input and select

--- app/code/Magecom/CheckoutStep/view/frontend/web/js/view/my-new-step.js
+++ app/code/Magecom/CheckoutStep/view/frontend/web/js/view/my-new-step.js
@@ -4,7 +4,6 @@ define(
         'uiComponent',
         'underscore',
         'Magento_Checkout/js/model/step-navigator',
-
     ],
     function (
         ko,
@@ -68,27 +67,6 @@ define(
             },
 
 
-
-            /**
-             * Form submit handler
-             *
-             * This method can have any name.
-             */
-            /*onSubmit: function() {
-                // trigger form validation
-                this.source.set('params.invalid', false);
-                this.source.trigger('customCheckoutForm.data.validate');
-
-                // verify that form data is valid
-                if (!this.source.get('params.invalid')) {
-                    // data is retrieved from data provider by value of the customScope property
-                    var formData = this.source.get('customCheckoutForm');
-                    // do something with form data
-                    console.dir(formData);
-                }
-            },*/
-
-
             /**
              * @returns void
              */
@@ -96,5 +74,6 @@ define(
                 stepNavigator.next();
             }
         });
+
     }
 );
