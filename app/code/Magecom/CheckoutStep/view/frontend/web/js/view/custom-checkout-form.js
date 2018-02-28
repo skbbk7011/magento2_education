a66e5f8ee11f07b87c85d33f703c83585bcbcaf8
Sofiia Khizhnia <sofabbk@gmail.com>
2018-02-28 15:32:47 +0200

    ko js

--- app/code/Magecom/CheckoutStep/view/frontend/web/js/view/custom-checkout-form.js
+++ app/code/Magecom/CheckoutStep/view/frontend/web/js/view/custom-checkout-form.js
@@ -1,53 +1,17 @@
-
-/*global define*/
-/*
-define([
-    'Magento_Ui/js/form/form'
-], function(Component) {
-    'use strict';
-    return Component.extend({
-        initialize: function () {
-            this._super();
-            // component initialization logic
-            return this;
-        },
-
-        /!**
-         * Form submit handler
-         *
-         * This method can have any name.
-         *!/
-        onSubmit: function() {
-            // trigger form validation
-            this.source.set('params.invalid', false);
-            this.source.trigger('customCheckoutForm.data.validate');
-
-            // verify that form data is valid
-            if (!this.source.get('params.invalid')) {
-                // data is retrieved from data provider by value of the customScope property
-                var formData = this.source.get('customCheckoutForm');
-                // do something with form data
-                console.dir(formData);
-            }
-        }
-    });
-});*/
-
-
+/**
+ * Copyright © Magento, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
 define(
     [
         'ko',
-        'uiComponent',
         'underscore',
-        'Magento_Checkout/js/model/step-navigator',
         'Magento_Ui/js/form/form'
     ],
     function (
         ko,
-        Component,
         _,
-        stepNavigator,
-        ComponentForm
+        Component
     ) {
         'use strict';
         /**
@@ -56,7 +20,7 @@ define(
          * <Vendor>_<Module>  - is the name of the your module directory.
          *
          */
-        return Component.extend({
+        /*return Component.extend({
             defaults: {
                 template: 'Magecom_CheckoutStep/mystep'
             },
@@ -64,10 +28,10 @@ define(
             //add here your logic to display step,
             isVisible: ko.observable(true),
 
-            /**
+            /!**
              *
              * @returns {*}
-             */
+             *!/
             initialize: function () {
                 this._super();
                 // register your step
@@ -83,41 +47,46 @@ define(
 
                     _.bind(this.navigate, this),
 
-                    /**
+                    /!**
                      * sort order value
                      * 'sort order value' < 10: step displays before shipping step;
                      * 10 < 'sort order value' < 20 : step displays between shipping and payment step
                      * 'sort order value' > 20 : step displays after payment step
-                     */
+                     *!/
                     1
                 );
 
                 return this;
             },
 
-            /**
+            /!**
              * The navigate() method is responsible for navigation between checkout step
              * during checkout. You can add custom logic, for example some conditions
              * for switching to your custom step
-             */
+             *!/
             navigate: function () {
 
             },
 
 
-            /**
+            /!**
              * @returns void
-             */
+             *!/
 
             navigateToNextStep: function () {
                 stepNavigator.next();
             }
-        });
-        return ComponentForm.extend({
+        });*/
+        return Component.extend({
+
+            isVisible: ko.observable(quote.isVirtual()),
+            inputName: ko.observable(true),
+
             initialize: function () {
                 this._super();
-                // component initialization logic
+
                 return this;
+
             },
 
             /**
@@ -125,9 +94,15 @@ define(
              *
              * This method can have any name.
              */
+
+
             onSubmit: function() {
                 // trigger form validation
                 this.source.set('params.invalid', false);
+                console.log(inputName);
+
+
+
                 this.source.trigger('customCheckoutForm.data.validate');
 
                 // verify that form data is valid
@@ -137,7 +112,6 @@ define(
                     // do something with form data
                     console.dir(formData);
                 }
-                Component.stepNavigator.next();
             }
         })
 
--- app/code/Magecom/CheckoutStep/view/frontend/web/js/view/my-new-step.js
+++ app/code/Magecom/CheckoutStep/view/frontend/web/js/view/my-new-step.js
@@ -1,25 +1,21 @@
+/**
+ * Copyright © Magento, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
 define(
     [
         'ko',
         'uiComponent',
         'underscore',
-        'Magento_Checkout/js/model/step-navigator',
-        'Magento_Ui/js/form/form'
+        'Magento_Checkout/js/model/step-navigator'
     ],
     function (
         ko,
         Component,
         _,
-        stepNavigator,
-        ComponentForm
+        stepNavigator
     ) {
         'use strict';
-        /**
-         *
-         * mystep - is the name of the component's .html template,
-         * <Vendor>_<Module>  - is the name of the your module directory.
-         *
-         */
         return Component.extend({
             defaults: {
                 template: 'Magecom_CheckoutStep/mystep'
@@ -34,60 +30,65 @@ define(
              */
             initialize: function () {
                 this._super();
-                // register your step
+
+                /*this.customerFirstName = ko.observable();
+                this.customerPhone = ko.observable();
+                this.customerDataYear = ["1999","2000","2001"];
+                this.customerYear = ko.observable();
+                this.validationKey = ko.observableArray();
+                this.fullName = ko.computed(function(){
+                    return this.customerFirstName() + " " + this.customerPhone();
+                }, this);
+
+                var self = this;
+                this.validationKey = function () {
+                    if(self.customerFirstName() != "" && self.customerPhone() != ""){
+                        return true;
+                    }
+                };*/
+
+
+
+
                 stepNavigator.registerStep(
-                    //step code will be used as step content id in the component template
                     'step_age',
-                    //step alias
                     null,
-                    //step title value
                     'Step Age',
-                    //observable property with logic when display step or hide step
                     this.isVisible,
-
                     _.bind(this.navigate, this),
-
-                    /**
-                     * sort order value
-                     * 'sort order value' < 10: step displays before shipping step;
-                     * 10 < 'sort order value' < 20 : step displays between shipping and payment step
-                     * 'sort order value' > 20 : step displays after payment step
-                     */
                     1
                 );
 
+
                 return this;
             },
-
-            /**
-             * The navigate() method is responsible for navigation between checkout step
-             * during checkout. You can add custom logic, for example some conditions
-             * for switching to your custom step
-             */
             navigate: function () {
 
             },
 
-
-            /**
-             * @returns void
-             */
             navigateToNextStep: function () {
                 stepNavigator.next();
+                /*console.log(this.customerPhone());
+                if (this.validationKey() == true){
+                    stepNavigator.next();
+                    console.log('no disabled');
+                } else {
+                    console.log('disabled');
+                }*/
             }
         });
-        return ComponentForm.extend({
+        /*return ComponentForm.extend({
             initialize: function () {
                 this._super();
                 // component initialization logic
                 return this;
             },
 
-            /**
+            /!**
              * Form submit handler
              *
              * This method can have any name.
-             */
+             *!/
             onSubmit: function() {
                 // trigger form validation
                 this.source.set('params.invalid', false);
@@ -101,6 +102,6 @@ define(
                     console.dir(formData);
                 }
             }
-        });
+        });*/
     }
 );
